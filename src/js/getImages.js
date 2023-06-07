eb98303ffa576d9afa1356383479ea87c9ccf794
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-06-07 20:39:44 +0300

    Add "try...catch" for getImages function

--- src/js/getImages.js
+++ src/js/getImages.js
@@ -5,29 +5,34 @@ import { buttonLoadMore, lightbox } from './index';
 
 //Функція, що фетчить та рендерить порцію картинок
 const getImages = async (query, pageToFetch) => {
-  const data = await fetchImages(query, pageToFetch);
-  if (!data.hits.length) {
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-    return;
-  }
-  renderImages(data);
-  //Викликаємо метод refresh бібліотеки simplelightbox після оновлення DOM
-  lightbox.refresh();
+  try {
+    const data = await fetchImages(query, pageToFetch);
+    if (!data.hits.length) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    renderImages(data);
+    //Викликаємо метод refresh бібліотеки simplelightbox після оновлення DOM
+    lightbox.refresh();
 
-  if (pageToFetch === 1) {
-    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-  }
+    if (pageToFetch === 1) {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
 
-  if (data.totalHits >= pageToFetch * pageLimit) {
-    buttonLoadMore.classList.remove('unvisible');
-  }
+    if (data.totalHits >= pageToFetch * pageLimit) {
+      buttonLoadMore.classList.remove('unvisible');
+    }
 
-  if (data.totalHits <= pageToFetch * pageLimit) {
-    Notiflix.Notify.info(
-      "We're sorry, but you've reached the end of search results"
-    );
+    if (data.totalHits <= pageToFetch * pageLimit) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results"
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure('Oops! Something went wrong!');
   }
 };
 
