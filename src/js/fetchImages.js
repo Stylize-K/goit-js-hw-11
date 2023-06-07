71e1da1a71488c11f0ff79886d8865e0836db880
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-06-07 04:15:05 +0300

    Create function "fetchImages" in separate file

---
+++ src/js/fetchImages.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+//Унікальний ключ доступу до API Pixabay
+const KEY = '37030220-55e5b35e4370d44ae057df5d9';
+
+//Ліміт отриманих об'ектів на сторінці
+const pageLimit = 40;
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+//Функція, що фетчить картинки
+const fetchImages = async (queryToFetch, pageToFetch) => {
+  try {
+    const { data } = await axios({
+      params: {
+        key: KEY,
+        q: queryToFetch,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: pageLimit,
+        page: pageToFetch,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure('Oops! Something went wrong!');
+  }
+};
+
+export { fetchImages, pageLimit };
